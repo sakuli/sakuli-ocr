7df4d6800969463550cd27ff90c93280ba2eaa89
Sven Hettwer <sven.hettwer@consol.de>
2020-11-23 11:28:42 +0100

    (#8) prettier + husky test

--- packages/sakuli-ocr/src/getTextFromRegion.ts
+++ packages/sakuli-ocr/src/getTextFromRegion.ts
@@ -2,23 +2,31 @@ import { Region } from "@sakuli/legacy";
 import { screen, Region as NutRegion } from "@nut-tree/nut-js";
 import { execSync } from "child_process";
 import { join } from "path";
-import fs from "fs"
+import fs from "fs";
 
-export async function getTextFromRegion(region: Region): Promise<string>{
+export async function getTextFromRegion(region: Region): Promise<string> {
+  const regionCapture = async (
+    selectedRegion: NutRegion,
+    outputFilename: string
+  ) => {
+    const regionImage = await (<any>screen).vision.grabScreenRegion(
+      selectedRegion
+    );
+    await (<any>screen).vision.saveImage(regionImage, outputFilename);
+  };
 
-    const regionCapture = async (selectedRegion: NutRegion, outputFilename: string) => {
-        const regionImage =  await (<any>screen).vision.grabScreenRegion(selectedRegion);
-        await (<any>screen).vision.saveImage(regionImage, outputFilename);
-    };
-
-    await regionCapture(
-        new NutRegion(
-            await region.getX() || 0,
-            await region.getY() || 0,
-            await region.getW() || 0,
-            await region.getH() || 0),
-        "ocr-screenshot.png");
-    const text = execSync(`tesseract ${join(process.cwd(), "ocr-screenshot.png")} stdout`)
-    fs.unlinkSync(join(process.cwd(), "ocr-screenshot.png"))
-    return Promise.resolve(text.toString())
+  await regionCapture(
+    new NutRegion(
+      (await region.getX()) || 0,
+      (await region.getY()) || 0,
+      (await region.getW()) || 0,
+      (await region.getH()) || 0
+    ),
+    "ocr-screenshot.png"
+  );
+  const text = execSync(
+    `tesseract ${join(process.cwd(), "ocr-screenshot.png")} stdout`
+  );
+  fs.unlinkSync(join(process.cwd(), "ocr-screenshot.png"));
+  return Promise.resolve(text.toString());
 }
