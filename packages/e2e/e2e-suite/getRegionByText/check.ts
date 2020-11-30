5fabd220987beda91e387acbe59c367e4e3f730a
Sven Hettwer <sven.hettwer@consol.de>
2020-11-30 13:30:01 +0100

    (#9) Added single word reverse ocr

--- packages/e2e/e2e-suite/getRegionByText/check.ts
+++ packages/e2e/e2e-suite/getRegionByText/check.ts
@@ -1,12 +1,12 @@
 (async () => {
   const testCase = new TestCase("getRegionByText");
-  const url = "https://sakuli.io";
+  const url = "https://sakuli.io/e2e-pages/fryed-egg/license.html";
   const screen = new Region();
   try {
     await _navigateTo(url);
     await testCase.endOfStep("Navigate to sakuli website");
-    await _getRegionByText("EXPLORE").click();
-    await _getRegionByText("Main Use-Cases").highlight(1);
+    await _getRegionByText("License").click();
+    // await _getRegionByText("Main Use-Cases").highlight(1);
   } catch (e) {
     await testCase.handleException(e);
   } finally {
--- packages/sakuli-ocr/src/getRegionByText.ts
+++ packages/sakuli-ocr/src/getRegionByText.ts
@@ -7,7 +7,7 @@ import {
   createThenableRegionClass,
 } from "@sakuli/legacy/dist/context/common";
 import fs from "fs";
-const jsdom = require("jsdom");
+const { JSDOM } = require("jsdom");
 
 export function getRegionByText(
   text: string,
@@ -32,7 +32,32 @@ export function getRegionByText(
     );
     fs.unlinkSync(screenshotPath);
 
-    const altoXml = new jsdom.JSDOM(altoXmlString);
+    const altoXml = new JSDOM(altoXmlString).window.document;
+    if(!text.includes(" ")){
+      const stringElementArray = Array.from(altoXml.querySelectorAll("String"));
+      let elementMatchingText;
+
+      for(let index = 0; index < stringElementArray.length; index++){
+        const currentElement = stringElementArray[index] as any;
+        const textContent = currentElement.attributes.getNamedItem("CONTENT").value;
+        if(text === textContent){
+          elementMatchingText = currentElement
+          break;
+        }
+      }
+
+      if(!elementMatchingText){
+        throw Error(`Could not find text "${text}" on screen.`)
+      }
+
+      const x = parseInt(elementMatchingText.attributes.getNamedItem("HPOS").value)
+      const y = parseInt(elementMatchingText.attributes.getNamedItem("VPOS").value)
+      const width = parseInt(elementMatchingText.attributes.getNamedItem("WIDTH").value)
+      const height = parseInt(elementMatchingText.attributes.getNamedItem("HEIGHT").value)
+      testExecutionContext.logger.debug(`Found text "${text}" on location {x: ${x}, y: ${y}, width: ${width}, height: ${height}}`)
+      return new ThenableRegionClass(x, y, width, height)
+    }
+
     return new ThenableRegionClass(10, 10, 10, 10);
   }
 
