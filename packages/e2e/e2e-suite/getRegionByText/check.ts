f9223e18e2214dd138ba1bdb7cd7622a1027a694
Sven Hettwer <sven.hettwer@consol.de>
2020-12-03 13:37:16 +0100

    (#9) improved E2E test

--- packages/e2e/e2e-suite/getRegionByText/check.ts
+++ packages/e2e/e2e-suite/getRegionByText/check.ts
@@ -1,16 +1,15 @@
 (async () => {
   const testCase = new TestCase("getRegionByText");
-  const url = "https://sakuli.io/e2e-pages/fryed-egg/license.html";
-  const screen = new Region();
+  const url = "https://sakuli.io/e2e-pages/sandbox/#";
   try {
     await _navigateTo(url);
-    await testCase.endOfStep("Navigate to sakuli website");
-    await _wait(1000)
-    await _getRegionByText("License").highlight(1)
-    await _getRegionByText("Software", new Region(1290, 300, 70,30)).highlight(1)
-    await _getRegionByText("Originally posted on CodePen").highlight(1)
-    await _wait(3000)
-    // await _getRegionByText("Main Use-Cases").highlight(1);
+    await testCase.endOfStep("Navigate to website");
+
+    await _getRegionByText("Every").highlight(1);
+    await testCase.endOfStep("find 'Every'");
+
+    await _getRegionByText("element in one place").highlight(1);
+    await testCase.endOfStep("find 'element in one place'");
   } catch (e) {
     await testCase.handleException(e);
   } finally {
