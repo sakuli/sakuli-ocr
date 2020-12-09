6f58db26d0e99f9d5518f7b5651dc467955bb7d0
Sven Hettwer <sven.hettwer@consol.de>
2020-12-09 11:31:54 +0100

    (#8) Altered e2e test

--- packages/e2e/e2e-suite/case1/check.ts
+++ packages/e2e/e2e-suite/case1/check.ts
@@ -1,20 +1,22 @@
 (async () => {
-    const testCase = new TestCase("Fry a typed egg");
-    const url = "https://sakuli.io/e2e-pages/fryed-egg/license.html";
-    const env = new Environment();
-    const screen = new Region()
-    try {
-        await _navigateTo(url);
-        await testCase.endOfStep("Navigate to sakuli website");
+  const testCase = new TestCase("Fry a typed egg");
+  const url = "https://sakuli.io/e2e-pages/sandbox/";
+  const env = new Environment();
+  const screen = new Region();
+  try {
+    await _navigateTo(url);
+    await testCase.endOfStep("Navigate to sakuli website");
 
-        await env.setSimilarity(.96)
-        const region = await screen.find("assets/lets-fry-text.png");
-        await region.highlight(1)
-        await _assertEqual(await _getTextFromRegion(region), "License Information\n\f")
-
-    } catch (e) {
-        await testCase.handleException(e);
-    } finally {
-        await testCase.saveResult();
-    }
+    await env.setSimilarity(0.96);
+    const region = await screen.find("assets/needle.png");
+    await region.highlight(1);
+    await _assertEqual(
+      await _getTextFromRegion(region),
+      "Every html element in one place. Just waiting to be styled.\n\f"
+    );
+  } catch (e) {
+    await testCase.handleException(e);
+  } finally {
+    await testCase.saveResult();
+  }
 })();
