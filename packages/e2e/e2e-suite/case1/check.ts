4d4871a239af8643240426f030dfa628b02e4cc1
Sven Hettwer <sven.hettwer@consol.de>
2020-12-09 11:49:28 +0100

    (#8) use fixed region instead of image recognition

--- packages/e2e/e2e-suite/case1/check.ts
+++ packages/e2e/e2e-suite/case1/check.ts
@@ -1,18 +1,17 @@
 (async () => {
   const testCase = new TestCase("Fry a typed egg");
   const url = "https://sakuli.io/e2e-pages/sandbox/";
-  const env = new Environment();
-  const screen = new Region();
   try {
     await _navigateTo(url);
-    await testCase.endOfStep("Navigate to sakuli website");
+    await testCase.endOfStep("Navigate to sakuli sandbox website");
 
-    await env.setSimilarity(0.8);
-    const region = await screen.find("assets/needle.png");
+    const region = await new Region(0, 0, 800, 300);
     await region.highlight(1);
-    await _assertEqual(
-      await _getTextFromRegion(region),
-      "Every html element in one place. Just waiting to be styled.\n\f"
+    const textOnScreen = await _getTextFromRegion(region);
+    const searchString = /Every html element in one place\. Just waiting to be styled\./;
+    await _assert(
+      Promise.resolve(!!textOnScreen.match(searchString)),
+      `Could not find searched text on screen: "${searchString}"`
     );
   } catch (e) {
     await testCase.handleException(e);
