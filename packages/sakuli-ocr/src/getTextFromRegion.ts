c0ea61fb66ea012a4331d7c7b5fa46407bce0ea5
Sven Hettwer <sven.hettwer@consol.de>
2020-12-09 16:00:43 +0100

    (#8) remove ocr screenshot again

--- packages/sakuli-ocr/src/getTextFromRegion.ts
+++ packages/sakuli-ocr/src/getTextFromRegion.ts
@@ -2,6 +2,7 @@ import { Region } from "@sakuli/legacy";
 import { Region as NutRegion, screen } from "@nut-tree/nut-js";
 import { execSync } from "child_process";
 import { join } from "path";
+import * as fs from "fs";
 
 export async function getTextFromRegion(region: Region): Promise<string> {
   const regionCapture = async (
@@ -26,6 +27,6 @@ export async function getTextFromRegion(region: Region): Promise<string> {
   const text = execSync(
     `tesseract ${join(process.cwd(), "ocr-screenshot.png")} stdout`
   );
-  // fs.unlinkSync(join(process.cwd(), "ocr-screenshot.png"));
+  fs.unlinkSync(join(process.cwd(), "ocr-screenshot.png"));
   return Promise.resolve(text.toString());
 }
