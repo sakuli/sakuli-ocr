fda0c2d9c80ab493164f17a900a46b78bd870061
Sven Hettwer <sven.hettwer@consol.de>
2020-12-10 11:56:05 +0100

    (#9) merged check.ts

--- packages/e2e/e2e-suite/getTextFromRegion/check.ts
+++ packages/e2e/e2e-suite/getTextFromRegion/check.ts
@@ -1,20 +1,22 @@
 (async () => {
   const testCase = new TestCase("getTextFromRegion");
-  const url = "https://sakuli.io/e2e-pages/fryed-egg/license.html";
-  const env = new Environment();
-  const screen = new Region();
+  const url = "https://sakuli.io/e2e-pages/sandbox/";
   try {
+    await testCase.startStep("Navigate to sakuli sandbox website");
     await _navigateTo(url);
-    await testCase.endOfStep("Navigate to sakuli website");
+    await _wait(1000);
 
-    await env.setSimilarity(0.96);
-    const region = await screen.find("assets/lets-fry-text.png");
-    await region.highlight(1);
-    await _assertEqual(
-      await _getTextFromRegion(region),
-      "License Information\n\f"
+    await testCase.startStep("get text from region");
+    const region = await new Region(0, 100, 650, 200);
+    const textOnScreen = await _getTextFromRegion(region);
+    const searchString = /Every html element in one place\. Just waiting to be styled\./;
+
+    Logger.logDebug(`Text on screen: \n ${textOnScreen}\n`);
+
+    await _assert(
+      Promise.resolve(!!textOnScreen.match(searchString)),
+      `Could not find searched text on screen: "${searchString}"`
     );
-    await region.mouseMove();
   } catch (e) {
     await testCase.handleException(e);
   } finally {
