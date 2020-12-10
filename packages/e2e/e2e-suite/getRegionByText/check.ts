3262074df5909bd3650b40c44397e74fef7c389c
Sven Hettwer <sven.hettwer@consol.de>
2020-12-10 12:06:05 +0100

    (#9) added wait and migrated to startStep

--- packages/e2e/e2e-suite/getRegionByText/check.ts
+++ packages/e2e/e2e-suite/getRegionByText/check.ts
@@ -2,14 +2,15 @@
   const testCase = new TestCase("getRegionByText");
   const url = "https://sakuli.io/e2e-pages/sandbox/#";
   try {
+    await testCase.startStep("Navigate to website");
     await _navigateTo(url);
-    await testCase.endOfStep("Navigate to website");
+    await _wait(1000);
 
+    await testCase.startStep("find 'Every'");
     await _getRegionByText("Every").highlight(1);
-    await testCase.endOfStep("find 'Every'");
 
+    await testCase.startStep("find 'element in one place'");
     await _getRegionByText("element in one place").highlight(1);
-    await testCase.endOfStep("find 'element in one place'");
   } catch (e) {
     await testCase.handleException(e);
   } finally {
