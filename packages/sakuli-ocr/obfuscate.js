bd7dd7cae0cc8f68915c825da0b39aa0da75a637
Sven Hettwer <sven.hettwer@consol.de>
2021-12-13 10:26:14 +0100

    (#18) removed sakuli license and code obfuscation

--- packages/sakuli-ocr/obfuscate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  compact: true,
-  selfDefending: true,
-  shuffleStringArray: true,
-  splitStrings: true,
-  stringArrayThreshold: 1
-};
--- packages/sakuli-ocr/src/index.ts
+++ packages/sakuli-ocr/src/index.ts
@@ -9,8 +9,6 @@ const ocrPreset: SakuliPresetProvider = (registry) => {
   registry.registerContextProvider(new OcrContextProvider());
 };
 
-export const pluginToken =
-  "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJjYXRlZ29yeSI6MTQsImlhdCI6MTYwNTUyNzQ4MSwiYXVkIjoiQHNha3VsaS9vY3IiLCJpc3MiOiJzYWt1bGkuaW8iLCJzdWIiOiJzYWt1bGlfcGx1Z2luIn0.9J2vFBNbGKm9ZezG5olu_uC6FjxNmCyLTVl44fV-MuO0aVXT6wjlgFjodsLqK7xXI8iqqb_0YunPhMsesSI9RA";
 export default ocrPreset;
 
 declare global {
